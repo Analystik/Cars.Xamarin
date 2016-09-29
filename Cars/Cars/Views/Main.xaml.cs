1ef7e39cb9e787904509eb84c7c2edc321994f55
Analystik <info@analystik.ca>
2016-09-29 18:41:07 -0400

    Makers, Province Binding

--- Cars/Cars/Views/Main.xaml.cs
+++ Cars/Cars/Views/Main.xaml.cs
@@ -8,6 +8,25 @@ using Xamarin.Forms;
 
 namespace Cars.Views
 {
+    public class xItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public xItem(int id, string title)
+        {
+            this.Id = id;
+            this.Title = title;
+        }
+
+        public override string ToString()
+        {
+            return this.Title;
+        }
+
+
+    }
+
+
     public partial class Main : ContentPage
     {
         public Main()
@@ -17,9 +36,33 @@ namespace Cars.Views
             var vm = new Cars.VModel();
             this.BindingContext = vm;
 
+            vm.PropertyChanged += Vm_PropertyChanged;
+
             vm.GetProvinces();
             vm.GetMakes();
         }
+
+        private void Vm_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            var vm = this.BindingContext as VModel;
+            switch (e.PropertyName)
+            {
+                case "Provinces":
+                    this.cboProvince.Items.Clear();
+                    foreach (var item in vm.Provinces)
+                        this.cboProvince.Items.Add(item.Name);
+                    break;
+                case "Makes":
+                    this.cboBrand.Items.Clear();
+                    foreach (var item in vm.Makes)
+                        this.cboBrand.Items.Add(item.Name);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+
 
         //void OnSliderValueChanged(object sender, ValueChangedEventArgs args) {
         //    lblKilometer.Text = ((Slider)sender).Value.ToString("F3");
@@ -27,8 +70,13 @@ namespace Cars.Views
 
         async void OnButtonClicked(object sender, EventArgs args)
         {
+            var vm = this.BindingContext as VModel;
+
+            await DisplayAlert("check", vm.Provinces[this.cboProvince.SelectedIndex].Name, "Cancel");
+
+
             //Button button = (Button)sender;
-            await Navigation.PushAsync(new Views.Result(new Data.Profile() { CarId = 1, ProvinceId = 1, KMPerYear = 10000 }));
+            //await Navigation.PushAsync(new Views.Result(new Data.Profile() { CarId = 1, ProvinceId = 1, KMPerYear = 10000 }));
         }
     }
 }
