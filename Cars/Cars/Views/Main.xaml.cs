cc94187b737998d6dedcf2a95ff48fbe0c1d3735
Analystik <info@analystik.ca>
2016-09-29 19:52:18 -0400

    Binding Done

--- Cars/Cars/Views/Main.xaml.cs
+++ Cars/Cars/Views/Main.xaml.cs
@@ -8,27 +8,14 @@ using Xamarin.Forms;
 
 namespace Cars.Views
 {
-    public class xItem
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public xItem(int id, string title)
-        {
-            this.Id = id;
-            this.Title = title;
-        }
-
-        public override string ToString()
-        {
-            return this.Title;
-        }
-
-
-    }
-
 
     public partial class Main : ContentPage
     {
+        private Data.Make SelectedMake;
+        private Data.Model SelectedModel;
+
+        private VModel ViewModel { get { return this.BindingContext as VModel; }  }
+
         public Main()
         {
             InitializeComponent();
@@ -38,24 +25,61 @@ namespace Cars.Views
 
             vm.PropertyChanged += Vm_PropertyChanged;
 
+            this.cboBrand.SelectedIndexChanged += CboBrand_SelectedIndexChanged;
+            this.cboModel.SelectedIndexChanged += CboModel_SelectedIndexChanged;
+
             vm.GetProvinces();
             vm.GetMakes();
         }
 
+
+        private void CboBrand_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.SelectedMake = null;
+            var index = this.cboBrand.SelectedIndex;
+
+            if (index > -1)
+                this.SelectedMake = this.ViewModel.Makes[index] as Data.Make;
+
+
+            this.ViewModel.GetModels(this.SelectedMake?.Id ?? 0);
+        }
+
+        private void CboModel_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.SelectedModel = null;
+            var index = this.cboModel.SelectedIndex;
+
+            if (index > -1)
+                this.SelectedModel = this.ViewModel.Models[index] as Data.Model;
+
+            this.ViewModel.GetCars(this.SelectedMake?.Id ?? 0, this.SelectedModel?.Id ?? 0);
+        }
+
         private void Vm_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
         {
-            var vm = this.BindingContext as VModel;
+            
             switch (e.PropertyName)
             {
-                case "Provinces":
-                    this.cboProvince.Items.Clear();
-                    foreach (var item in vm.Provinces)
-                        this.cboProvince.Items.Add(item.Name);
-                    break;
                 case "Makes":
                     this.cboBrand.Items.Clear();
-                    foreach (var item in vm.Makes)
+                    foreach (var item in this.ViewModel.Makes)
                         this.cboBrand.Items.Add(item.Name);
+                    break;
+                case "Models":
+                    this.cboModel.Items.Clear();
+                    foreach (var item in this.ViewModel.Models)
+                        this.cboModel.Items.Add(item.Name);
+                    break;
+                case "Cars":
+                    this.cboCar.Items.Clear();
+                    foreach (var item in this.ViewModel.Cars)
+                        this.cboCar.Items.Add(item.Year4Digits.ToString());
+                    break;
+                case "Provinces":
+                    this.cboProvince.Items.Clear();
+                    foreach (var item in this.ViewModel.Provinces)
+                        this.cboProvince.Items.Add(item.Name);
                     break;
                 default:
                     break;
@@ -63,20 +87,17 @@ namespace Cars.Views
         }
 
 
-
-        //void OnSliderValueChanged(object sender, ValueChangedEventArgs args) {
-        //    lblKilometer.Text = ((Slider)sender).Value.ToString("F3");
-        //}
-
         async void OnButtonClicked(object sender, EventArgs args)
         {
-            var vm = this.BindingContext as VModel;
+            var car = this.ViewModel.Cars[this.cboCar.SelectedIndex] as Data.Car;
+            var province = this.ViewModel.Provinces[this.cboProvince.SelectedIndex] as Data.Province;
 
-            await DisplayAlert("check", vm.Provinces[this.cboProvince.SelectedIndex].Name, "Cancel");
+            var profile = new Data.Profile();
+            profile.CarId = car?.Id ?? 0;
+            profile.KMPerYear = Convert.ToInt32(this.sldKilometer.Value);
+            profile.ProvinceId = province?.Id ?? 0;
 
-
-            //Button button = (Button)sender;
-            //await Navigation.PushAsync(new Views.Result(new Data.Profile() { CarId = 1, ProvinceId = 1, KMPerYear = 10000 }));
+            await Navigation.PushAsync(new Views.Result(profile));
         }
     }
 }
